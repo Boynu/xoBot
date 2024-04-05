8f405fdbab2ddaa191ca1c2175d4459327343bd8
Boynu <139995517+Boynu@users.noreply.github.com>
2024-04-05 16:23:09 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv('API_ID'))
-API_HASH = os.getenv('API_HASH')
-SESSION_NAME = os.getenv('SESSION_NAME')
-XO_BOT = int(os.getenv('XO_BOT'))
+API_ID = int(os.getenv('API_ID', "12210813"))
+API_HASH = os.getenv('API_HASH', "e42eeae11a2f96bcfc5ec3b46a30adad")
+SESSION_NAME = os.getenv('SESSION_NAME', "AgC6Un0AYLixlmKmIJfi9ins0pzmI92zLqn_ZfKANsEtXo4z-NG_2PipD33yDssfhY_-esYBl52E87IWC2ENV04TWpAqdNnU7NjDDyPa-RKTZjD9M0X-0G_sj5mmcBwzoqk0P3JxJqg8TuDLl7melnbaZ4RqsB8h4BRJs_R6EYZN85ddcO3N9hqS1GhEyO7x51jztPhtiGgj0v0h1BgnuYrPFaQFQYympDT3W-rZfvtM2iEl5o59Fi_AqAeqxkryZgm6WPAl2Z81CVXs8q069_WlBFeRmK4zxid5VPO_16fIeBgLMi2UXVQQze3zzVY0jTwYRSFeZoQdEliBFrJeVUuTKrL6bgAAAAFRk3SoAA")
+XO_BOT = int(os.getenv('XO_BOT', "XAOS_Tagbot"))
